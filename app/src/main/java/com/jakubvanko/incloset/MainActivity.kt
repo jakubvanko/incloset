c9faa196bb773ef6e752f1e63f6b223f21b0a222
jakubvanko <kubko.vanko@gmail.com>
2023-03-15 17:10:49 +0100

    feat: add basic firebase auth support

--- app/src/main/java/com/jakubvanko/incloset/MainActivity.kt
+++ app/src/main/java/com/jakubvanko/incloset/MainActivity.kt
@@ -1,43 +1,93 @@
 package com.jakubvanko.incloset
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.jakubvanko.incloset.ui.theme.InclosetTheme
 
 class MainActivity : ComponentActivity() {
+    private var user: FirebaseUser? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            InclosetTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            MainView()
+        }
+    }
+
+    private fun signIn() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    private val signInLauncher =
+        registerForActivityResult(FirebaseAuthUIActivityResultContract()) { res ->
+            this.signInResult(res)
+        }
+
+    private fun signInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            user = FirebaseAuth.getInstance().currentUser
+        } else {
+            Log.e("MainActivity.kt", "Error logging in " + response?.error?.errorCode)
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun MainView() {
+        InclosetTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                Button(onClick = { signIn() }) {
+                    Text(text = "Login")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    InclosetTheme {
-        Greeting("Android")
+fun Navigation() {
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf("Overview", "Manage", "Preferences")
+    Box(modifier = Modifier.fillMaxSize()) {
+        NavigationBar(
+            modifier = Modifier.align(Alignment.BottomEnd)
+        ) {
+            items.forEachIndexed { index, item ->
+                NavigationBarItem(
+                    icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+                    label = { Text(item) },
+                    selected = selectedItem == index,
+                    onClick = { selectedItem = index }
+                )
+            }
+        }
     }
 }
